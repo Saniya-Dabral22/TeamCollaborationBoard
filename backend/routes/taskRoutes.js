8a21d643db3a6bdc8a9ae01ce75f5ddbf71272e6
Saniya-Dabral22 <saniyadabral334@gmail.com>
2025-06-20 14:19:44 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const taskController = require('../controllers/taskController');
+const taskController = require('../controller/taskController');
 
 router.put('/:taskId', taskController.updateTask);
 router.delete('/:taskId', taskController.deleteTask);
